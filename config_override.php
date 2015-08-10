30bde21248931b0acc880efe10aaa986f692dcc0
lnklnklnk <alexander.eroshkin@gmail.com>
2015-08-10 18:43:44 +0300

    initial commit

---
+++ config_override.php
@@ -0,0 +1,12 @@
+<?php
+/***CONFIGURATOR***/
+$sugar_config['dbconfigoption']['collation'] = 'utf8_general_ci';
+$sugar_config['default_currency_iso4217'] = 'RUB';
+$sugar_config['default_currency_name'] = 'Rubles';
+$sugar_config['default_currency_symbol'] = '₽';
+$sugar_config['default_date_format'] = 'd.m.Y';
+$sugar_config['default_decimal_seperator'] = ',';
+$sugar_config['default_locale_name_format'] = 'f l';
+$sugar_config['default_number_grouping_seperator'] = '';
+$sugar_config['default_time_format'] = 'H:i';
+/***CONFIGURATOR***/
