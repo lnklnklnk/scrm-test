6fe76cfe080ffe5d93b6bd8fa9ef5b4b13b1083d
lnklnklnk <alexander.eroshkin@gmail.com>
2015-08-13 11:41:45 +0300

    Dashlet draft

---
+++ modules/fox_Task/Dashlets/fox_TaskContactsDistribution/fox_TaskContactsDistibution.php
@@ -0,0 +1,90 @@
+<?php
+if(!defined('sugarEntry') || !sugarEntry) die('Not A Valid Entry Point');
+/*********************************************************************************
+ * SugarCRM Community Edition is a customer relationship management program developed by
+ * SugarCRM, Inc. Copyright (C) 2004-2013 SugarCRM Inc.
+ * 
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Affero General Public License version 3 as published by the
+ * Free Software Foundation with the addition of the following permission added
+ * to Section 15 as permitted in Section 7(a): FOR ANY PART OF THE COVERED WORK
+ * IN WHICH THE COPYRIGHT IS OWNED BY SUGARCRM, SUGARCRM DISCLAIMS THE WARRANTY
+ * OF NON INFRINGEMENT OF THIRD PARTY RIGHTS.
+ * 
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE.  See the GNU Affero General Public License for more
+ * details.
+ * 
+ * You should have received a copy of the GNU Affero General Public License along with
+ * this program; if not, see http://www.gnu.org/licenses or write to the Free
+ * Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+ * 02110-1301 USA.
+ * 
+ * You can contact SugarCRM, Inc. headquarters at 10050 North Wolfe Road,
+ * SW2-130, Cupertino, CA 95014, USA. or at email address contact@sugarcrm.com.
+ * 
+ * The interactive user interfaces in modified source and object code versions
+ * of this program must display Appropriate Legal Notices, as required under
+ * Section 5 of the GNU Affero General Public License version 3.
+ * 
+ * In accordance with Section 7(b) of the GNU Affero General Public License version 3,
+ * these Appropriate Legal Notices must retain the display of the "Powered by
+ * SugarCRM" logo. If the display of the logo is not reasonably feasible for
+ * technical reasons, the Appropriate Legal Notices must display the words
+ * "Powered by SugarCRM".
+ ********************************************************************************/
+
+
+require_once('include/Dashlets/Dashlet.php');
+
+
+class fox_TaskContactsDistibution extends Dashlet {
+
+    var $height = '200'; // height of the pad
+
+
+
+    /**
+     * Displays the dashlet
+     *
+     * @return string html to display dashlet
+     */
+    function display() {
+        $ss = new Sugar_Smarty();
+
+        $ss->assign('id', $this->id);
+        $ss->assign('height', $this->height);
+
+        $str = $ss->fetch('modules/fox_Task/Dashlets/fox_TaskContactsDistibution/fox_TaskContactsDistibution.tpl');
+        return parent::display($this->dashletStrings['LBL_DBLCLICK_HELP']) . $str . '<br />'; // return parent::display for title and such
+    }
+
+    /**
+     * Displays the javascript for the dashlet
+     *
+     * @return string javascript to use with this dashlet
+     */
+
+    /**
+     * Displays the configuration form for the dashlet
+     *
+     * @return string html to display form
+     */
+
+    /**
+     * called to filter out $_REQUEST object when the user submits the configure dropdown
+     *
+     * @param array $req $_REQUEST
+     * @return array filtered options to save
+     */
+
+
+    /**
+     * Used to save text on textarea blur. Accessed via Home/CallMethodDashlet.php
+     * This is an example of how to to call a custom method via ajax
+     */
+
+}
+
+?>
