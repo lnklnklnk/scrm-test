4b9f690db9715c2a897eb9c616848b35fe8abca2
lnklnklnk <alexander.eroshkin@gmail.com>
2015-08-13 20:17:33 +0300

    d

---
+++ custom/Extension/modules/fox_Task/Ext/Layoutdefs/fox_usertask_fox_task_fox_Task.php
@@ -0,0 +1,23 @@
+<?php
+ // created: 2015-08-13 19:15:00
+$layout_defs["fox_Task"]["subpanel_setup"]['fox_usertask_fox_task'] = array (
+  'order' => 100,
+  'module' => 'fox_UserTask',
+  'subpanel_name' => 'default',
+  'sort_order' => 'asc',
+  'sort_by' => 'id',
+  'title_key' => 'LBL_FOX_USERTASK_FOX_TASK_FROM_FOX_USERTASK_TITLE',
+  'get_subpanel_data' => 'fox_usertask_fox_task',
+  'top_buttons' => 
+  array (
+    0 => 
+    array (
+      'widget_class' => 'SubPanelTopButtonQuickCreate',
+    ),
+    1 => 
+    array (
+      'widget_class' => 'SubPanelTopSelectButton',
+      'mode' => 'MultiSelect',
+    ),
+  ),
+);
---
+++ custom/modules/fox_Task/Ext/Vardefs/vardefs.ext.php
@@ -0,0 +1,17 @@
+<?php 
+ //WARNING: The contents of this file are auto-generated
+
+
+// created: 2015-08-13 19:15:00
+$dictionary["fox_Task"]["fields"]["fox_usertask_fox_task"] = array (
+  'name' => 'fox_usertask_fox_task',
+  'type' => 'link',
+  'relationship' => 'fox_usertask_fox_task',
+  'source' => 'non-db',
+  'module' => 'fox_UserTask',
+  'bean_name' => 'fox_UserTask',
+  'side' => 'right',
+  'vname' => 'LBL_FOX_USERTASK_FOX_TASK_FROM_FOX_USERTASK_TITLE',
+);
+
+?>
